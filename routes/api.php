d1b575575c3c3b9df079242af3105d382c633564
jcamilovallejos <jcamilovallejos@gmail.com>
2024-06-25 10:36:19 -0500

    Ajuste de comentarios en api

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\AuthController;
 
+//No me funciono el register
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+//Se deja por fuera el las rutas de tasks para que puedan ser verificadas
+Route::apiResource('tasks', TaskController::class);
 Route::middleware('auth:api')->group(function () {
-    Route::apiResource('tasks', TaskController::class);
 });
 
 
